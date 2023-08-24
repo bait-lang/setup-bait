c1e90e77f8b180054bf45bdcd9f909531c74d7b4
Lukas Neubert <serkonda7@pm.me>
2023-08-24 20:51:03 +0200

    bait.exe --> bait.bat

--- src/setup-bait.js
+++ src/setup-bait.js
@@ -3,20 +3,20 @@ const exec = require('@actions/exec')
 
 async function run() {
 	try {
-		await exec.exec('git', ['clone', 'https://github.com/tiabeast/bait.git', './bait'])
+		await exec.exec('git', ['clone', 'https://github.com/tiabeast/bait.git', 'bait'])
+		process.chdir('bait')
 
-		process.chdir('./bait')
 		if (process.platform === 'linux') {
 			await exec.exec('./make.sh')
 			await exec.exec('./bait', ['symlink'])
 		} else if (process.platform === 'win32') {
-			await exec.exec('make.bat')
-			await exec.exec('./bait.exe', ['symlink'])
+			await exec.exec('./make.bat')
+			await exec.exec('./bait.bat', ['symlink'])
 		} else {
 			core.setFailed('Unsupported platform: ' + process.platform)
 		}
 
-		core.setOutput('bait-path', './bait')
+		core.setOutput('bait-path', './bait.bat')
 		core.setOutput('version', 'x.x.x') // TODO get dynamically
 	} catch (error) {
 		core.setFailed(error.message)
