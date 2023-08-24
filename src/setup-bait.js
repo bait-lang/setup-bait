2b62b50b994385e0fdd4e34485f92f783aa98411
Lukas Neubert <serkonda7@pm.me>
2023-08-24 17:19:22 +0200

    prepare for platform agnostic code

--- src/setup-bait.js
+++ src/setup-bait.js
@@ -1,19 +1,23 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
+const core = require('@actions/core')
+const exec = require('@actions/exec')
 
 async function run() {
 	try {
 		await exec.exec('git', ['clone', 'https://github.com/tiabeast/bait.git', './bait'])
 
 		process.chdir('./bait')
-		await exec.exec('./make.sh')
-		await exec.exec('./bait', ['symlink'])
+		if (process.platform === 'linux') {
+			await exec.exec('./make.sh')
+			await exec.exec('./bait', ['symlink'])
+		} else {
+			core.setFailed('Unsupported platform: ' + process.platform)
+		}
 
 		core.setOutput('bait-path', './bait')
 		core.setOutput('version', 'x.x.x') // TODO get dynamically
 	} catch (error) {
-		core.setFailed(error.message);
+		core.setFailed(error.message)
 	}
 }
 
-run();
+run()
